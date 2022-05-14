b50f857dc77efc97a9b07d8f3ffa6276cd5eeeba
Almir Motika <Almir_18B@hotmail.com>
2022-05-14 16:41:32 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 import './App.scss';
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+
+import {BrowserRouter,Route} from 'react-router-dom';
+import Header from './Components/header/Header';
+import Footer from './Components/footer/Footer';
+
+import Routes from './config/Routes';
+
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+    <Route render={props=>(
+      <>
+      <Header {...props}/>
+      <Routes/>
+      <Footer/>
+      </>
+    )}/>
+    </BrowserRouter>
   );
 }
 

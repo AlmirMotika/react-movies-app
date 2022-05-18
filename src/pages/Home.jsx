a44d881d53d3ecdbc04cb0d173bdad9a2f05c1b9
Almir Motika <Almir_18B@hotmail.com>
2022-05-19 00:54:04 +0200

    Home page - Movie list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import HeroSlide from '../Components/hero-slide/Heroslide'
+import { OutlineButton } from '../Components/button/Button'
+import{category,movieType,tvType} from '../api/tmdbApi'
+import MovieList from '../Components/movie-list/MovieList'
+
 const Home = () => {
   return (
     <>
     <HeroSlide/>
+    <div className="container">
+      <div className="section mb-3">
+        <div className="section__header mb-2">
+          <h2>Trending movies</h2>
+          <Link to="/movie">
+            <OutlineButton className="small">View more</OutlineButton>
+          </Link>
+        </div>
+    <MovieList category={category.movie} type={movieType.popular}/>
+      </div>
+
+      <div className="section mb-3">
+        <div className="section__header mb-2">
+          <h2>Trending movies</h2>
+          <Link to="/movie">
+            <OutlineButton className="small">View more</OutlineButton>
+          </Link>
+        </div>
+    
+      </div>
+
+      <div className="section mb-3">
+        <div className="section__header mb-2">
+          <h2>Trending movies</h2>
+          <Link to="/movie">
+            <OutlineButton className="small">View more</OutlineButton>
+          </Link>
+        </div>
+    
+      </div>
+
+      <div className="section mb-3">
+        <div className="section__header mb-2">
+          <h2>Trending movies</h2>
+          <Link to="/movie">
+            <OutlineButton className="small">View more</OutlineButton>
+          </Link>
+        </div>
+    
+      </div>
+    </div>
     </>
   )
 }

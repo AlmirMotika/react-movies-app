ac0fa3e03d09553446a10f8413396c11662649ab
Almir Motika <Almir_18B@hotmail.com>
2022-05-20 17:54:12 +0200

    Make Catalog Page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import PageHeader from '../Components/page-header/PageHeader';
+import {category as cate} from '../api/tmdbApi';
 
 const Catalog = () => {
+  const {category}=useParams();
+
+  console.log(category);
   return (
-    <div>
-      Catalog
-    </div>
-  )
+    <>
+    <PageHeader>
+      {category===cate.movie?'Movie':'TV Shows'}
+    </PageHeader>
+    </>
+  );
 }
 
 export default Catalog
